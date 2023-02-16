f7409674051892202616addba941092ba1bb5dc7
FZill <florian.zill@ufz.de>
2023-02-16 11:30:28 +0100

    added initial heat production from filling of repo

--- src/glaciationBCs/repoclass_AREHS.py
+++ src/glaciationBCs/repoclass_AREHS.py
@@ -38,21 +38,22 @@ class repo():
 		self.t_filled = t_filled
 		self.t_prev = 0.
 		
-	def radioactive_heatflow(self,t): # heat flow = Wärmestrom
+	def radioactive_heatflow(self,t_sim): # heat flow = Wärmestrom
 
-		Q_BE = lambda t: np.sum(self.BE_Q[0:4]*np.exp(-t@self.BE_z[np.newaxis,0:4]))
-		Q_HA = lambda t: np.sum(self.HA_Q[0:4]*np.exp(-t@self.HA_z[np.newaxis,0:4]))
-		# stepwise filling of RK-BE
-		Qsum_BE = np.sum(Q_BE(t-np.arange(0,80,4)[:,np.newaxis]*s_a + self.t_inter_BE))
-		Qsum_HA = np.sum(Q_HA(t-np.arange(0,80,4)[:,np.newaxis]*s_a + self.t_inter_HA))
+		Q_BE = lambda t: np.sum(self.BE_Q[:]*np.exp(-t@self.BE_z[np.newaxis,:]))
+		Q_HA = lambda t: np.sum(self.HA_Q[:]*np.exp(-t@self.HA_z[np.newaxis,:]))
+		# stepwise filling of repository in steps of 4 years
+		# time array which masks out containers which are not yet stored
+		t_masked = t_sim-np.arange(0, self.t_filled, 4*s_a)
+		t_masked = t_masked[t_masked >= 0]
+		Qsum_BE = np.sum(Q_BE(t_masked[:, np.newaxis] + self.t_inter_BE))
+		Qsum_HA = np.sum(Q_HA(t_masked[:, np.newaxis] + self.t_inter_HA))
 
 		return Qsum_BE*self.BE_vol*self.BE_f + Qsum_HA*self.HA_vol*self.HA_f
 
 	def radioactive_heatflux(self,t): # heat flux = Wärmestromdichte!
-		# shift time to when the dgr is filled completely
-		shifted_t = self.t_filled + t
-		
-		return self.radioactive_heatflow(shifted_t) / self.repo_size
+		# use t + self.t_filled to disregard initial heat production from filling
+		return self.radioactive_heatflow(t) / self.repo_size
 
 
 	# auxiliary functions
@@ -61,15 +62,14 @@ class repo():
 		print(self.radioactive_heatflow(self.t_filled), "W")
 
 	def plot_evolution(self):
-		time_ = np.linspace(self.t_filled , 10000*s_a, 1000)
+		time_ = np.linspace(0, 800, 201)*s_a
 
 		plt.figure(figsize=(12,6))
 		plt.title('Gesamtwärmeleistung der Abfälle (RK-BE + RK-HA)\n nach vollständiger Einlagerung')
 		plt.plot(time_/s_a, [self.radioactive_heatflow(ti)/1000 for ti in time_],
 			label='RK-BE + RK-HA stufenweise befüllt', color = 'red', lw = 2.5)
-		plt.semilogx()
-		plt.xlim(10,10000)
-		plt.ylim(0,13000)
+		#plt.xlim(0,10000)
+		#plt.ylim(0,13000)
 		plt.xlabel('Zeit [a]')
 		plt.ylabel('Leistung [kW]')
 		plt.legend(loc='upper right')
